9445db83d6711a3fa1d0b7d487ff072cbb516ac6
SW33TSTUFF <simaakniyaz@gmail.com>
2024-07-13 20:33:34 +0530

    Update

--- React Files/assignment1/src/App.js
+++ React Files/assignment1/src/App.js
@@ -1,16 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome';
-import Hello from './components/Hello'
-import Message from './components/Message';
-import Counter from './components/Counter';
+// import Greet from './components/Greet'
+// import Welcome from './components/Welcome';
+// import Hello from './components/Hello'
+// import Message from './components/Message';
+// import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Jim" heroName="Browning" />
-      <Welcome name="Bruce" heroName="Wayne" />
+      {/* <Greet name="Jim" heroName="Browning" />
+      <Welcome name="Bruce" heroName="Wayne" /> */}
+      {/* <FunctionClick />
+      <ClassClick /> */}
+      {/* <EventBind /> */}
+      <ParentComponent />
 
     </div >
 
